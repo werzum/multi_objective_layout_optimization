e40c3506388e90a9debce3abd654d9c22e8d627d
Carl Orge Retzlaff <e.awerg@web.de>
2022-07-25 17:06:42 +0200

    correct slope_line plotting

--- plotting.py
+++ plotting.py
@@ -2,7 +2,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 def plot_gdfs(gdfs):
-    fig, ax = plt.subplots(figsize = (20,16)) 
+    fig, ax = plt.subplots(figsize = (20,16))
+    ax.set_xlabel('X coordinate', fontsize=15)
+    ax.set_ylabel('Y coordinate', fontsize=15)
     for gdf in gdfs:
         gdf.plot(alpha=0.5,ax=ax)
 
@@ -10,3 +12,11 @@ def plot_scatter_xy(list):
     xs = [point.x for point in list]
     ys = [point.y for point in list]
     plt.scatter(xs, ys)
+
+def plot_equal_axis(line):
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.set_xlabel('X coordinate', fontsize=15)
+    ax.set_ylabel('Y coordinate', fontsize=15)
+    ax.plot(*line.xy, label='LineString')
+    ax.axis('equal')
+    return ax
