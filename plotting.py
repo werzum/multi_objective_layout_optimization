5cfca71c5b745ef219c3ac3c411fa180c25bd7a5
Carl Orge Retzlaff <e.awerg@web.de>
2022-07-26 11:09:06 +0200

    Reworked interactive plotting to semi-automated state

--- plotting.py
+++ plotting.py
@@ -2,7 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 def plot_gdfs(gdfs):
-    fig, ax = plt.subplots(figsize = (20,16))
+    fig, ax = plt.subplots()
     ax.set_xlabel('X coordinate', fontsize=15)
     ax.set_ylabel('Y coordinate', fontsize=15)
     for gdf in gdfs:
@@ -21,31 +21,19 @@ def plot_equal_axis(line):
     ax.axis('equal')
     return ax
 
-def onclick_coords(data):
+def onclick(event):
     """Return an interactive figure to record mouse clicks on their coordinates
-        Creates and modifies a global coords variable to store the clicked points in
+        Modifies a global coords variable to store the clicked points in
     Args:
-        data (list): A list of the data to be plotted
+        event (_type_): event fired by fig
     """    
-    def onclick(event):
-        global ix, iy
-        ix, iy = event.xdata, event.ydata
+    global ix, iy
+    ix, iy = event.xdata, event.ydata
 
-        global coords
-        coords.append((ix, iy))
-        print("Coordinates recorded are:",coords)
-
-        if len(coords) == 8:
-            coords = []
-            fig.canvas.mpl_disconnect(cid)
-            print("Canvas disconnected due to full array")
-    
-    # ensure that we start with an empty array
     global coords
-    coords = []
+    coords.append((ix, iy))
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(data[0],data[1])
-
-    cid = fig.canvas.mpl_connect('button_press_event', onclick)
+    if len(coords) == 6:
+        print("Coordinates recorded are:",coords)
+        coords = []
+        print("Array reset")
