7824e7762b8238f49d350262edac0cde9068a519
Carl Orge Retzlaff <e.awerg@web.de>
2022-07-26 09:47:05 +0200

    Recording clicked coordinates in interactive figure

--- plotting.py
+++ plotting.py
@@ -20,3 +20,32 @@ def plot_equal_axis(line):
     ax.plot(*line.xy, label='LineString')
     ax.axis('equal')
     return ax
+
+def onclick_coords(data):
+    """Return an interactive figure to record mouse clicks on their coordinates
+        Creates and modifies a global coords variable to store the clicked points in
+    Args:
+        data (list): A list of the data to be plotted
+    """    
+    def onclick(event):
+        global ix, iy
+        ix, iy = event.xdata, event.ydata
+
+        global coords
+        coords.append((ix, iy))
+        print("Coordinates recorded are:",coords)
+
+        if len(coords) == 8:
+            coords = []
+            fig.canvas.mpl_disconnect(cid)
+            print("Canvas disconnected due to full array")
+    
+    # ensure that we start with an empty array
+    global coords
+    coords = []
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(data[0],data[1])
+
+    cid = fig.canvas.mpl_connect('button_press_event', onclick)
