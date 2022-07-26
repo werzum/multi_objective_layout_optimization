da8cdc62496e353571582585c6ad413e40dca97b
Carl Orge Retzlaff <e.awerg@web.de>
2022-07-26 16:06:18 +0200

    Fix onClick Global plotting and some more tests with MOO

--- plotting.py
+++ plotting.py
@@ -12,6 +12,7 @@ def plot_scatter_xy(list):
     xs = [point.x for point in list]
     ys = [point.y for point in list]
     plt.scatter(xs, ys)
+    plt.show()
 
 def plot_equal_axis(line):
     fig, ax = plt.subplots(figsize=(10, 10))
@@ -21,7 +22,7 @@ def plot_equal_axis(line):
     ax.axis('equal')
     return ax
 
-def onclick(event):
+def onclick(event, coords):
     """Return an interactive figure to record mouse clicks on their coordinates
         Modifies a global coords variable to store the clicked points in
     Args:
@@ -30,10 +31,33 @@ def onclick(event):
     global ix, iy
     ix, iy = event.xdata, event.ydata
 
-    global coords
     coords.append((ix, iy))
 
     if len(coords) == 6:
         print("Coordinates recorded are:",coords)
         coords = []
         print("Array reset")
+
+def plot_optimization(target_function, results, xy):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    im = ax.imshow(target_function(xy), interpolation='bilinear', origin='lower',
+                cmap='gray')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+
+    def plot_point(res, marker='o', color=None):
+        ax.plot(512+res.x[0], 512+res.x[1], marker=marker, color=color, ms=10)
+
+    # SHGO produces multiple minima, plot them all (with a smaller marker size)
+    plot_point(results['shgo'], color='r', marker='+')
+    plot_point(results['shgo_sobol'], color='r', marker='x')
+
+    for i in range(results['shgo_sobol'].xl.shape[0]):
+        ax.plot(512 + results['shgo_sobol'].xl[i, 0],
+                512 + results['shgo_sobol'].xl[i, 1],
+                'ro', ms=2)
+
+    ax.set_xlim([-4, 514*2])
+    ax.set_ylim([-4, 514*2])
+    plt.show()
