Fixed computation of productivity costs, checks out now

src/main/classes_linear_optimization.py
@@ -615,10 +615,7 @@ def model_results_comparison(
         distance_carriage_support_array.append(
             np.sum(result.c2f_vars * distance_carriage_support)
         )
-
-        # get the total cable road costs
-        total_cable_road_costs = np.sum(result.fac_vars * facility_cost)
-        cable_road_costs.append(total_cable_road_costs)
+        cable_road_costs.append(np.sum(result.fac_vars * facility_cost))
 
         # subtract the productivity cost from the total profit
         total_profit_here = (
@@ -627,7 +624,7 @@ def model_results_comparison(
             - total_cable_road_costs
         )
 
-        cost_objective.append(productivity_array[-1] + total_cable_road_costs)
+        cost_objective.append(productivity_array[-1] + cable_road_costs[-1])
         overall_profit.append(total_profit_here)
 
         ecological_distances.append(result.ecological_objective)

src/main/interface.py
@@ -213,11 +213,20 @@ def update_layout_overview(indices, forest_area_3, model_list) -> dict:
         distance_trees_to_lines_sum = sum(distance_tree_line)
 
     # compute the productivity cost
-    productivity_cost_overall = np.sum(
-        model_list[0].productivity_cost[
-            range(len(tree_to_line_assignment)), tree_to_line_assignment
-        ]
-    )
+    selected_prod_cost = model_list[0].productivity_cost[:, indices]
+    productivity_cost_overall = 0
+    for index, val in enumerate(tree_to_line_assignment):
+        print(index, val)
+        productivity_cost_overall += selected_prod_cost[index][val]
+
+    # productivity_cost_overall = np.sum(
+    #     model_list[0].productivity_cost[indices, :][
+    #         range(len(tree_to_line_assignment)), tree_to_line_assignment
+    #     ]
+    # )
+    # print(model_list[0].productivity_cost[indices, :])
+    # print(tree_to_line_assignment)
+    # print(productivity_cost_overall)
 
     # sum of wood volume per CR
     # here we need to compute the sum of wood per tree to line assignment to return this for the CR table